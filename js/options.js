e58dc428f7ffa7706b7c40802c9651f23db63702
胡凯 <hukai@hukaideMacBook.local>
2020-11-08 09:50:06 +0800

    update

--- js/options.js
+++ js/options.js
@@ -5,24 +5,49 @@ function init_library(){
 var engine_library={
   "搜索引擎":{
     'baidu':'https://www.baidu.com/s?wd={%s}',
-		'google':'https://www.google.com/search?q={%s}'
+		'google':'https://www.google.com/search?q={%s}',
+    'bing':'https://cn.bing.com/search?q={%s}',
+    'duckduckgo':'https://duckduckgo.com/?q={%s}',
+    'wikipedia':'https://en.wikipedia.org/w/index.php?search={%s}'
   },
   "建筑案例":{
     'gooood': 'https://www.gooood.cn/search/{%s}',
   	'archdaily': 'https://www.archdaily.cn/search/cn/all?q={%s}',
 		'archcollege':'http://www.archcollege.com/home.php?m=Index&type=article&a=search&key={%s}',
     'archiposition':'http://www.archiposition.com/?s={%s}&cat=all',
-    'ikuku':'http://www.ikuku.cn/?s={%s}'
+    'ikuku':'http://www.ikuku.cn/?s={%s}',
+    'designboom':'http://www.designboom.cn/news/search.php?kw={%s}',
+    'architizer':'https://architizer.com/projects/q/q:{%s}',
+    'dezeen':'https://www.dezeen.com/?s={%s}',
+    'socks':'http://socks-studio.com/?s={%s}',
+    'beta':'http://www.beta-architecture.com/?s=search&q={%s}',
+    'nikken':'https://www.nikken.co.jp/cn/search/result.html?query={%s}'
   },
   "图片搜索":{
     'pinterest':'https://www.pinterest.com/search/pins/?q={%s}',
 		'huaban':'https://huaban.com/search/?q={%s}',
 		'500px':'https://500px.com/search?q={%s}&type=photos',
-		'unsplash':'https://unsplash.com/s/photos/{%s}}'
+		'unsplash':'https://unsplash.com/s/photos/{%s}}',
+    'dazuo':'https://www.bigbigwork.com/tupian/?w={%s}',
+    'zcool':'https://www.zcool.com.cn/search/content?&word={%s}',
+    'thenounproject':'https://thenounproject.com/search/?q={%s}',
+    'iconfont':'https://www.iconfont.cn/search/index?searchType=icon&q={%s}',
+    'behance':'https://www.behance.net/search?search={%s}',
+    'dribbble':'https://dribbble.com/search/{%s}'
   },
-  "社交平台":{
+  "平台搜索":{
     'zhihu':'https://www.zhihu.com/search?type=content&q={%s}',
-    'weibo':'https://s.weibo.com/weibo/{%s}'
+    'weibo':'https://s.weibo.com/weibo/{%s}',
+    'weixin':'https://weixin.sogou.com/weixin?type=2&query={%s}',
+    'github':'https://github.com/search?q={%s}',
+    'douban':'https://www.douban.com/search?q={%s}'
+  },
+  "学术搜索":{
+    "zhiwang":"https://search.cnki.com.cn/Search/Result?content={%s}",
+    "baidu_scholar":"https://xueshu.baidu.com/s?wd={%s}",
+    "google_scholar":"https://scholar.google.com/scholar?q={%s}",
+    "weipu":"http://www.cqvip.com/main/search.aspx?k={%s}",
+    "wanfang":"http://www.wanfangdata.com.cn/search/searchList.do?searchType=all&searchWord={%s}"
   }
 	}
 // init engine_library
@@ -43,7 +68,7 @@ chrome.storage.sync.get("search_engine",function(result){
             $(".setting_box").append($(this));
           }else{
             $(this).find('span').text("+");
-            $(this).find('span').attr({style:"background:#3498db"});
+            $(this).find('span').attr({style:"background:#7d6cfc"});
             $(".category[data-category='xxx']".replace('xxx',$(this).attr("data-category"))).append($(this));
           }
         });
